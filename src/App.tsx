c276e15ae45c5fd2afb7483afdc488575adc2633
9Diin <weartstudio@naver.com>
2025-07-14 16:52:04 +0900

    🔥 [feature] 토픽 - Hot Topic 카드 UI 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 
 // 페이지 컴포넌트
-import HomePage from "./pages/Home";
+import HomePage from "./pages/home/Home";
 import SignInPage from "./pages/Sign-in";
 import SignUpPage from "./pages/Sign-up";
+import CreatePage from "./pages/home/Create";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/new-topic" element={<CreatePage />} />
                     <Route path="/sign-in" element={<SignInPage />} />
                     <Route path="/sign-up" element={<SignUpPage />} />
                 </Routes>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 import AppHeader from "@/components/common/AppHeader";
 import AppFooter from "@/components/common/AppFooter";
 import SideBar from "@/components/home/side-bar";
-import { Flame, Frame, PencilLine } from "lucide-react";
+import HotTopicCard from "@/components/home/card/hot-topic";
 import NewTopicCard from "@/components/home/card/new-topic";
 
+import { Flame, PencilLine } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
+    const navigate = useNavigate();
+
     return (
-        <div className="page">
+        <div className="page absolute">
             <AppHeader />
             <div className="container">
                 <div className="w-full h-full flex items-start p-6 gap-6">
@@ -15,7 +22,7 @@ function HomePage() {
                     {/* 토픽 콘텐츠 UI */}
                     <div className="w-4/5 flex flex-col gap-12">
                         {/* 핫 토픽 */}
-                        <div>
+                        <div className="flex flex-col gap-6">
                             <div className="flex flex-col gap-1">
                                 <div className="flex items-center gap-2">
                                     <Flame />
@@ -23,7 +30,12 @@ function HomePage() {
                                 </div>
                                 <p className="text-neutral-500">지금 가장 주목받는 주제들을 살펴보고, 다양한 관점의 인사이트를 얻어보세요.</p>
                             </div>
-                            <div className="grid grid-cols-4"></div>
+                            <div className="grid grid-cols-4 gap-4">
+                                <HotTopicCard />
+                                <HotTopicCard />
+                                <HotTopicCard />
+                                <HotTopicCard />
+                            </div>
                         </div>
                         {/* NEW 토픽 */}
                         <div className="flex flex-col gap-6">
@@ -45,6 +57,10 @@ function HomePage() {
                 </div>
             </div>
             <AppFooter />
+            <Button className="fixed bottom-8" onClick={() => navigate("/new-topic")}>
+                <PencilLine />
+                토픽 작성하기
+            </Button>
         </div>
     );
 }
